5f1c8bdf2023466d867d33a610356333e4e07271
Muhammad Abdullah Cheema <abdullahchema419@gmail.com>
2026-09-10 18:39:57 +0000

    variables

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Abdullah cheema");
+const accountId = 12345
+var accountEmail = "abdullah8cheema@gmail.com"
+let accountpass = "123456789"
+username = "AbdullahCheema"
+
+// mainly hm jo data type istemal krte hain na woah const or let hi krte hain var nahi krte kyon ke woah boht old hai or uske scope ka issue hai
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountpass , username]);
+//we can reinitialize var or let only 
+ accountEmail = "abdullah cheema47@gmail.com"
+ accountpass = "12345"
+username="Abdulah"
+console.table([accountId , accountEmail ,accountpass , username ]);
