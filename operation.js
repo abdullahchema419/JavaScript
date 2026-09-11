4b5293a3c37a4dcbeb213fe426fe8562b7295285
Muhammad Abdullah Cheema <abdullahchema419@gmail.com>
2026-09-11 11:40:05 +0000

    operations

---
+++ operation.js
@@ -0,0 +1,12 @@
+//here we study aabout operators 
+// let str1="hello"
+// let str2=" Abdullah"
+// let str3=str1 + str2;
+// console.log(str3);
+// yeah to simple tha ab hemain dekhna yeah haike agr hemare pas 2 value hain aik to string hai or second number usko aksie add krain ge 
+console.log("1" + 2)
+console.log(1 + "2");
+// inmain presidence hoge to yeah string hi show hoga hemain 
+//  isi trhan agr hm aesa krain to
+// console.log("1" + 2 + 2)// ismain bhi string bena ge kyon ke string hemare pas pahle hai
+console.log(1+2+"2")//ismain aesa hoga number pahle hain to first owah add honge uske bad jakr yeah add string bane gui means uska answer hemare pas hoga 132
