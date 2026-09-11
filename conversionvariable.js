be6ae53fefbf3bd7b15abc762fc290011abaa56c
Muhammad Abdullah Cheema <abdullahchema419@gmail.com>
2026-09-11 10:53:08 +0000

    conversiondatatype

---
+++ conversionvariable.js
@@ -0,0 +1,27 @@
+//ismian hm conversion sekhain ge ke aik datatype se dosre datatype main hm kaise conversion krsakte hain 
+
+// let score =33;
+// yehan hemare pas score jo hai woah number datatype main hai
+// hm yeah chahte hain ke aik ho string jaise ke 
+// let score ="33";
+// yeah hemare pas string hai hm chhate hain ke isko hm number main change krdain to hm krain ge aesa ke
+// let numberScore =Number(score);//yeah hoea change number main
+// console.log(typeof numberScore);
+// but ab hm change hain ke 
+let score = "33abc";//isko change krain number main to
+let numberscore=Number(score)//to yeah show to krega ke number main hogea ha but iske value NaN show hoge which mean Not a Number
+//  console.log(typeof numberscore);
+//  console.log(numberscore)
+
+// same isi trhan hm scrint or bool main change krskate hain
+
+let istrue=1;
+// console.log(typeof istrue);
+// yeah number show hogw agr isko bool main chnage krna hoto
+
+let isbool = Boolean(istrue);
+// console.log(typeof(isbool)) ab change hoge bool main
+console.log(isbool);
+// true hoga to agr hm khali string karain ge woah false hoe agr kuch bhi usmain daldain ge to true or number bhi zero ke ilawa koi to true hoge 
+ 
+
