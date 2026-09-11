2d6972fe007459ad35696f5c2a3852ee9b68c2cd
Muhammad Abdullah Cheema <abdullahchema419@gmail.com>
2026-09-11 10:12:33 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,16 @@
+// here we discuss about datatype
+//  major datatypes are
+// number => jismain hm maximum  2 to power 52 tk save krwasakte hain
+// bigint => jismain hm large value store krsakte hain joke number se bhara hota hai
+// string =>""  ismain hm string value store krwasakte hain
+// boolean => ismain hm true/false store krwa sakte hain 
+// null => null aik standalone value hai agr kisi jegah 0 bhi aik value hote hai jaise temperature udhr hm null use krte hain
+// undefined => yeah hm wehan use krte hain jn hm variable ko koi value assign nahi krte to woah uske datatype hote hai
+// symbol  => unique
+
+
+// iske bad hemare pas aik function hota hai typeof jo hemain variable ke data type betata hain ex  
+
+console.log(typeof undefined);// undefined ke type undefined hi hote hai 
+console.log( typeof age);// age ke type hemae pas number hoge
+console.log(typeof null); // null ke type hemare pas object hoge 
