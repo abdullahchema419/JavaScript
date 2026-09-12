2d01d4ebc18e33c3ca27351f362ff945054b8dd4
Muhammad Abdullah Cheema <abdullahchema419@gmail.com>
2026-09-12 09:09:45 +0000

    string

--- string.js
+++ string.js
@@ -1,3 +1,12 @@
 //string can be donated by both sigle or double quote can be use with both
 
-//concat
+//concat best method
+// first method 
+// use old console.log method 
+const name="Abdullah"
+const repoCount=50;
+/*
+console.log(name+repoCount+"value"); */
+//Second method is use baktic this is advance way to use 
+//string interpolation
+console.log(`My name is ${name} . My repo Count is ${repoCount}`);
