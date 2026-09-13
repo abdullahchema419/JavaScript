9702e3c90db101026107890a521e1db42ecfff24
Muhammad Abdullah Cheema <abdullahchema419@gmail.com>
2026-09-13 19:35:47 +0000

    function

--- functions.js
+++ functions.js
@@ -39,14 +39,14 @@
 function calculateCartPrice(...num1){
     return num1
 }
-console.log(calculateCartPrice(2,3,4))//ismain hmisse jitne marzi oytput lesakte hain
+// console.log(calculateCartPrice(2,3,4))//ismain hmisse jitne marzi oytput lesakte hain
 
 // aik or case ai hemare aps
 
 function calculateCartPrice2(val1 ,val2 , ...num1){
     return val1 
 }
-console.log(calculateCartPrice2(2,3,4,5,6))//ismain hmisse jitne marzi oytput lesakte hain 
+// console.log(calculateCartPrice2(2,3,4,5,6))//ismain hmisse jitne marzi oytput lesakte hain 
 // first val1 and val2 main woahi store honge first  remaining sare vale num1 main store honge
 
 //ab hm dekhain ge ke object ko hm function main kaise store krwa skate hain 
@@ -57,7 +57,7 @@ console.log(calculateCartPrice2(2,3,4,5,6))//ismain hmisse jitne marzi oytput le
 // }
 
 function calculateCartPrice3(anyobject){
-console.log(`${anyobject.username} has to pay ${anyobject.price}`);
+// console.log(`${anyobject.username} has to pay ${anyobject.price}`);
  }
 // calculateCartPrice3(user)  
 
@@ -69,7 +69,7 @@ calculateCartPrice3( {username : "Abdullah" , price : 100 } )
 // isi trhan hm function main array bhi dal skate hian 
 const myArr = [1,2,3,4,5]
 function calculateCartPrice4(anyarray){
-    console.log(anyarray[2]);
+    // console.log(anyarray[2]);
 }
 // console.log(calculateCartPrice4(myArr))
 
@@ -78,4 +78,21 @@ function calculateCartPrice4(anyarray){
 function calculateCartPrice5(anyarray){  
     return anyarray[2]
 }
-console.log(calculateCartPrice5([1,2,3,4,5]))//isme hmne direct array pass kra hai function main    
+// console.log(calculateCartPrice5([1,2,3,4,5]))//isme hmne direct array pass kra hai function main    
+
+
+
+
+// ++++++++++++ 
+// ab hm aik newchwz dekhain ke function aik or reka se bhi declair hojata hai
+addone(5);
+function addone(num){
+    return num+1
+}  //ismian koi issue nahi hai kyon ke ismain hemain sai result dega normal exexute kia hai
+
+
+
+addtwo(5);//yeh function abhi run ho rha hai but hmne isko declare nahi kra hai to ye error dega  , inke ismethod main error dega kyon ke isko uper declair kia hia 
+const addtwo = function(num){
+    return num+2
+}
